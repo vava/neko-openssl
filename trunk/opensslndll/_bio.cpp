2ccc53a7511cbc158ea7367b12a9eef47f612758
Khurgin <Khurgin@e775d338-441e-0410-bce7-4d2ff2883aa1>
2006-09-28 14:37:59 +0000

    --first version of haxe secure connection - not works=)

--- trunk/opensslndll/_bio.cpp
+++ trunk/opensslndll/_bio.cpp
@@ -9,7 +9,8 @@ DEFINE_KIND(k_pointer);
 //BIO *BIO_new_connect(char* host_port);
 
 value _BIO_new_connect(value host_port) {
-	printf("[string:%s][strlen:%d][is_string:%d]",val_string(host_port),
+	// 4Debug
+	printf("Debug: [string:%s][strlen:%d][is_string:%d]",val_string(host_port),
 											val_strlen(host_port),val_is_string(host_port));
 	void* ptr;
 	//try {
@@ -65,6 +66,17 @@ value _BIO_write(value b, value data, value len) {
 	return alloc_int(BIO_write( (BIO*)val_data(b), val_data(data), val_int(len) ));
 }
 
+//#define BIO_set_conn_hostname(b,name) BIO_ctrl(b,BIO_C_SET_CONNECT,0,(char *)name)
+value _BIO_set_conn_hostname(value b, value name) {
+	return alloc_best_int((BIO*) val_data(b), (char*) val_data(name) );
+}
+
+//void	BIO_free_all(BIO *a);
+value _BIO_free_all(value a) {
+	BIO_free_all((BIO*) val_data(a) );
+	return VAL_VOID;
+}
+
 //Register
 DEFINE_PRIM(_ERR_load_BIO_strings, 0);
 DEFINE_PRIM(_BIO_new_connect, 1);
@@ -72,3 +84,5 @@ DEFINE_PRIM(_BIO_do_connect, 1);
 DEFINE_PRIM(_BIO_read, 3);
 DEFINE_PRIM(_BIO_set_conn_port, 2);
 DEFINE_PRIM(_BIO_write, 3);
+DEFINE_PRIM(_BIO_set_conn_hostname, 2);
+DEFINE_PRIM(_BIO_free_all, 1);
--- trunk/opensslndll/_ssl.cpp
+++ trunk/opensslndll/_ssl.cpp
@@ -5,8 +5,8 @@
 #include "openssl/ssl.h"
 #include "openssl/bio.h"
 #include "openssl/err.h"
-
-
+DEFINE_KIND(k_ssl_ctx_pointer);
+DEFINE_KIND(k_ssl_method_pointer);
 
 //void	SSL_load_error_strings(void );
 value _SSL_load_error_strings() {
@@ -20,7 +20,64 @@ value _OpenSSL_add_all_algorithms() {
 	return VAL_VOID;
 }
 
-//Register
+//int SSL_library_init(void );
+value _SSL_library_init(){
+	SSL_library_init();
+	return VAL_VOID;
+}
+//SSL_CTX *SSL_CTX_new(SSL_METHOD *meth);
+value _SSL_CTX_new(value meth){
+	return alloc_abstract(k_ssl_ctx_pointer,SSL_CTX_new((SSL_METHOD*)val_data(meth)));
+}
+//int SSL_CTX_load_verify_locations(SSL_CTX *ctx, const char *CAfile,
+//	const char *CApath);
+value _SSL_CTX_load_verify_locations(value ctx, /*value CAfile, */ value CApath){
+	return alloc_int(
+		SSL_CTX_load_verify_locations((SSL_CTX*)val_data(ctx),
+											/*val_string(CAfile)*/ NULL,
+											val_string(CApath))
+		);
+}
+//BIO *BIO_new_ssl_connect(SSL_CTX *ctx);
+value _BIO_new_ssl_connect(value ctx){
+	return alloc_abstract(k_ssl_ctx_pointer,BIO_new_ssl_connect((SSL_CTX*)val_data(ctx)));
+}
+//#define BIO_get_ssl(b,sslp)	BIO_ctrl(b,BIO_C_GET_SSL,0,(char *)sslp)
+//long	BIO_ctrl(BIO *bp,int cmd,long larg,void *parg);
 
+value _BIO_get_ssl(value b, value sslp){
+	
+	int a1234 = 2;
+	int b1234 = 3;
+	int c________ = a1234+b1234;
+	SSL* ssl = (SSL*)val_data(sslp);
+	return alloc_best_int(BIO_get_ssl((BIO*)val_data(b), & ssl));
+}
+
+//#define SSL_set_mode(ssl,op) SSL_ctrl((ssl),SSL_CTRL_MODE,(op),NULL)
+//long	SSL_ctrl(SSL *ssl,int cmd, long larg, void *parg);
+value _SSL_set_mode(value ssl, value op) {
+	return alloc_best_int(SSL_set_mode((SSL*) val_data(ssl), val_int(op)));
+}
+
+//#define SSL_MODE_AUTO_RETRY 0x00000004L
+value _SSL_MODE_AUTO_RETRY() {
+	return  alloc_best_int(0x00000004L);
+}
+
+//SSL_METHOD *SSLv23_client_method(void);
+value _SSLv23_client_method() {
+	return alloc_abstract(k_ssl_method_pointer, SSLv23_client_method());
+}
+
+//Register
+DEFINE_PRIM(_BIO_get_ssl,2);
+DEFINE_PRIM(_BIO_new_ssl_connect,1);
+DEFINE_PRIM(_SSL_CTX_load_verify_locations,2) //!!
+DEFINE_PRIM(_SSL_CTX_new,1);
+DEFINE_PRIM(_SSL_library_init,0);
 DEFINE_PRIM(_SSL_load_error_strings, 0);
 DEFINE_PRIM(_OpenSSL_add_all_algorithms, 0);
+DEFINE_PRIM(_SSL_set_mode, 2);
+DEFINE_PRIM(_SSL_MODE_AUTO_RETRY, 0);
+DEFINE_PRIM(_SSLv23_client_method, 0);
