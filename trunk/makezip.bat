ba61093d509d7e7384b429d976fc429a50ede7dc
berjevan <berjevan@e775d338-441e-0410-bce7-4d2ff2883aa1>
2006-10-18 10:31:50 +0000

    bat

--- trunk/makezip.bat
+++ trunk/makezip.bat
@@ -1,1 +1,25 @@
-winrar a -r -E *.svn openssl.zip ./openssl/*.*
+del openssl.zip
+mkdir temp
+cd ./temp
+mkdir openssl
+cd openssl
+xcopy ..\..\haxe /S /-Y
+xcopy ..\..\haxelib.xml
+mkdir src
+mkdir ndll
+cd ndll
+mkdir Windows
+mkdir Linux
+cd Windows
+xcopy ..\..\..\..\debug\openssl.ndll
+cd ..\Linux
+xcopy ..\..\..\..\ndll_linux\openssl.ndll
+cd ..\..\src
+mkdir opensslndll
+mkdir ndlltest
+cd opensslndll
+xcopy ..\..\..\..\opensslndll
+cd ..\ndlltest
+xcopy ..\..\..\..\ndlltest
+cd ..\..\..\..
+winrar a -r -ep1 -x*\.svn\* -x*.user openssl.zip ./temp/*.*
