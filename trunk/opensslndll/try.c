8ed4dfca4e8c9baaefcc549cfb1e129d3b784181
berjevan <berjevan@e775d338-441e-0410-bce7-4d2ff2883aa1>
2006-10-24 07:31:35 +0000

    -- C branch in trunk

---
+++ trunk/opensslndll/try.c
@@ -0,0 +1,12 @@
+#include "test.h"
+#include "neko.h"
+#include "stdio.h"
+#include "_hmac.h"
+
+int main() {
+	int x;
+	n_hello();
+	x = mcon();
+	printf ("mcon response = [%d]", x);
+	return 0;
+}
